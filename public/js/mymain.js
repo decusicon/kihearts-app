9ec3c68a82559bdf29ea9cd90612fffec71714f8
Success Chukwu <chuxsuccess0@gmail.com>
2020-05-23 06:32:27 +0100

    photos are displayed on each campaign, also i have set a dynamic select tag value setter.

--- public/js/mymain.js
+++ public/js/mymain.js
@@ -1,6 +1,7 @@
+// CLOSE FLASH MESSAGES
 function closeFlash() {
   var selector = "body #messages";
-  if ($(selector).children().hasClass("stubborn")) {
+  if (!$(selector).children().hasClass("stubborn")) {
     if ($(selector).children().hasClass("error"))
       setInterval(() => {
         $(selector).hide();
@@ -21,6 +22,7 @@ function closeFlash() {
 }
 closeFlash();
 
+// GENERATE ALERT BOXES
 function genAlertBox(type, msg) {
   var html = `<div id="messages">
     <ul class="${type}">
@@ -31,6 +33,7 @@ function genAlertBox(type, msg) {
   $("body").prepend(html);
 }
 
+// CLEAR EVERY MODAL INPUT
 function clearAllInput() {
   // When "Close" button is clicked in any modal, all input should get EMPTY.
   $(`[data-dismiss="modal"]`).click(() => {
@@ -41,40 +44,270 @@ function clearAllInput() {
 }
 // clearAllInput();
 
-function getUserCountry() {
-  var select = document.querySelectorAll("#country");
-  var options = document.querySelectorAll("#country > option");
-  options.forEach((option) => {
-    var optionValue = option.value.toLowerCase();
-    var country = document.getElementById("real_country").innerText;
-    if (optionValue == country) {
-      option.setAttribute("selected", "selected");
-      select.value = country;
+// CREATE CAMPAIGN MODAL
+function createCampaignModal() {
+  let createCampaignBtn = $(".createCampaignBtn");
+
+  // At click event, do the following.
+  createCampaignBtn.click(() => {
+    // Empty all inputs
+    $(".createCampaign input").val("");
+
+    // Change Modal Title
+    $(".createCampaign #createCampaignTitle").text("Create Campaign");
+
+    // Change "modalAlert" content
+    $(".createCampaign #modalAlert").html(
+      `You're about to create a crowdfunding fund campaign,
+      remember you are to grow the campaign to 20,000 GC before it can be
+      <span class="text-success">published</span>.`
+    );
+
+    // Change Modal Buttons
+    $(".createCampaign #createCampaignSumbitBtn").text("Create Campaign");
+    $(".createCampaign #deleteCampaignBtn").addClass("hide");
+
+    // Change Modal Form Action
+    $(".createCampaign #createCampaignForm").attr(
+      "action",
+      "/campaigns/create"
+    );
+  });
+
+  // Selection of Category
+  $(".createCampaign select#category").on("input", () => {
+    var category = document.querySelector("#category");
+    var subCategory = $("#subCategory");
+
+    // All Category Array
+    const categoriesArr = [
+      {
+        mainCat: "BUSINESS",
+        subCat: [
+          "Business Capital",
+          "Business Expansion Fund",
+          "Manufacturing Equipment",
+        ],
+      },
+      {
+        mainCat: "CHARITY",
+        subCat: [
+          "Motherless Baby Home",
+          "Old Peoples Home",
+          "Orphanage Home",
+          "Physically Challenged",
+        ],
+      },
+      {
+        mainCat: "DISASTER",
+        subCat: ["Earthquake", "Fire", "Flood", "Volcanic Eruptions", "War"],
+      },
+      {
+        mainCat: "EDUCATION",
+        subCat: [
+          "Books/E-books",
+          "Degree Program",
+          "Information Technology Program",
+          "Scholarship",
+          "Tuition Fee",
+        ],
+      },
+      { mainCat: "EMERGENCY", subCat: ["Emergency"] },
+      {
+        mainCat: "ENTERTAINMENT",
+        subCat: [
+          "Anniversary",
+          "Award",
+          "Baby Naming",
+          "Birthday",
+          "Burial",
+          "Wedding",
+        ],
+      },
+      {
+        mainCat: "HEALTH",
+        subCat: [
+          "Drugs",
+          "Food",
+          "Hospital Bill",
+          "Lab Test",
+          "Red Cross",
+          "Scan",
+        ],
+      },
+      {
+        mainCat: "PROJECT",
+        subCat: [
+          "Building Project",
+          "Community Project",
+          "Medical Research",
+          "School Project",
+          "Technological Research",
+        ],
+      },
+      {
+        mainCat: "PROPERTY",
+        subCat: [
+          "Car",
+          "Cloth",
+          "Electronics",
+          "House",
+          "Land",
+          "Shoes",
+          "Upholstery",
+        ],
+      },
+      {
+        mainCat: "RENT",
+        subCat: [
+          "Rent Car",
+          "Rent Equipment",
+          "Rent Hall",
+          "Rent Home",
+          "Rent Hotel",
+          "Rent Office",
+          "Rent Shop",
+        ],
+      },
+      {
+        mainCat: "REPAIR",
+        subCat: [
+          "Repair Apartment",
+          "Repair Electronics",
+          "Repair Generator",
+          "Repair Machine",
+          "Repair Motorbike",
+          "Repair Vehicle",
+        ],
+      },
+      {
+        mainCat: "SPORTS",
+        subCat: [
+          "American Football",
+          "Baseball",
+          "Basketball",
+          "Bike Race",
+          "Boxing",
+          "Car Race",
+          "Cricket",
+          "Football",
+          "Handball",
+          "Ice Hockey",
+          "Lawn Tennis",
+          "Rugby",
+          "Swimming",
+          "Table Tennis",
+          "Track & Field",
+          "Volleyball",
+        ],
+      },
+      {
+        mainCat: "TRAVEL",
+        subCat: [
+          "Flight",
+          "Freight",
+          "International Passport",
+          "Rail",
+          "Road",
+          "Visa",
+        ],
+      },
+      { mainCat: "OTHERS", subCat: ["Others"] },
+    ];
+
+    for (const child of category.children) {
+      if (category.value === child.value) {
+        categoriesArr.forEach((Cat) => {
+          if (Cat.mainCat === category.value) {
+            subCategory.html(""); // Empty subCategory
+
+            // Append "Select a Sub Category" option before any other options
+            subCategory.append(
+              `<option value="">Select a Sub Category</option>`
+            );
+
+            // Looping through to append "subCat" strings to subCategory options
+            Cat.subCat.forEach((subCats) => {
+              subCategory.append(
+                `<option value="${subCats}">${subCats}</option>`
+              );
+            });
+          }
+          if (category.value === "") {
+            subCategory.html(""); // Empty subCategory
+
+            // Append "Select a Sub Category" option before any other options
+            subCategory.append(
+              `<option value="">Select a Sub Category</option>`
+            );
+          }
+        });
+      }
     }
   });
 }
-getUserCountry();
+editCampaignModal();
 
-function getNextCountry() {
-  var select = document.querySelectorAll("#next_country");
-  var options = document.querySelectorAll("#next_country > option");
-  options.forEach((option) => {
-    var optionValue = option.value.toLowerCase();
-    var country = document.getElementById("real_next_country").innerText;
-    if (optionValue == country) {
-      option.setAttribute("selected", "selected");
-      select.value = country;
-    }
+// EDIT CAMPAIGN MODAL
+function editCampaignModal() {
+  let editCampaignBtn = $(".editCampaignBtn");
+
+  // At click event, do the following.
+  editCampaignBtn.click(() => {
+    // Empty all inputs
+    $(".createCampaign input").val("");
+
+    // Change Modal Title
+    $(".createCampaign #createCampaignTitle").text("Edit Campaign");
+
+    // Change "modalAlert" content
+    $(".createCampaign #modalAlert").html(
+      `Remember, when this campaign is 
+      <span class="text-success">published</span>, 
+      you can't edit it!`
+    );
+
+    // Change Modal Buttons
+    $(".createCampaign #createCampaignSumbitBtn").text("Save Changes");
+    $(".createCampaign #deleteCampaignBtn").removeClass("hide");
+
+    // Change Modal Form Action
+    $(".createCampaign #createCampaignForm").attr(
+      "action",
+      `/campaigns/edit/${window.user.id}`
+    );
   });
 }
-getNextCountry();
+createCampaignModal();
 
+// GET USER'S COUNTRY
+function setSelect(selector) {
+  var select = document.querySelectorAll(selector);
+  if (select.length) {
+    var options = document.querySelectorAll(`${selector} > option`);
+    var selectvalue = select[0].dataset.selectvalue;
+    var optionvalue = "";
+
+    options.forEach((option) => {
+      optionvalue = option.value.toLowerCase();
+      if (optionvalue == selectvalue) {
+        option.setAttribute("selected", "selected");
+        select.value = selectvalue;
+      }
+    });
+  }
+}
+setSelect("#country");
+setSelect("#next_country");
+
+// CLEAR HISTORY WHEN LOGGED OUT
 function clearHistoryOnLogout(url) {
   history.state = {};
   sessionStorage.clear();
   window.location.replace(url);
 }
 
+// PREVIEW AVATAR WJILE UPLOADING
 function previewAvatar() {
   $("#avatar").change((e) => {
     var file = e.target.files[0];
@@ -88,6 +321,7 @@ function previewAvatar() {
 }
 previewAvatar();
 
+// COUNTDOWN TIMER
 function countdowntimer(selector) {
   var countdownId = document.getElementById(selector);
   if (countdownId != null) {
@@ -124,6 +358,7 @@ function countdowntimer(selector) {
 }
 countdowntimer("countdown");
 
+// DROPZONE PHOTOS UPLOAD
 function dropzoneCon() {
   const setParamName = () => {
     return "photo";

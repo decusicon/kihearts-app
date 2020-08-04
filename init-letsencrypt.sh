729237b0c3625dcb2b24c63ce7389f3884dd992a
wizguy007 <akinwizguy@gmail.com>
2020-08-04 04:50:34 +0100

    generate docker compose for dev

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(kihearts.net www.kihearts.net app.kihearts.net)
+domains=(kihearts.net www.kihearts.net *.kihearts.net)
 rsa_key_size=4096
 data_path="./docker-compose/certbot"
 email="akinwizguy@gmail.com" # Adding a valid address is strongly recommended
